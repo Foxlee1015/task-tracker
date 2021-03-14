2461dd70cffcd036d98ecf16844ec464c59b71f9
Daehan <dh16931@gmail.com>
2021-03-14 19:42:08 +0900

    chore: add cors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import time
 from flask import Flask
+from flask_cors import CORS
 
 from apis import blueprint as api
 from core.db import init_db, backup_db
@@ -20,6 +21,7 @@ def background_task():
 
 def create_app():
     app = Flask(__name__)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.register_blueprint(api, url_prefix='/api')
     # api.init_app(app) 
     # https://flask-restplus.readthedocs.io/en/stable/scaling.html
